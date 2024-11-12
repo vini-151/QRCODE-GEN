2a24be7c0c827af35450dc9739142dadc9f54ab0
vini-151 <ovini.15515@gmail.com>
2024-11-12 16:02:59 -0300

    primeiro commit: começando config do prompt

---
+++ src/prompts/prompt_main.js
@@ -0,0 +1,9 @@
+const mainPompt = [
+    {
+        name: "select",
+        description: "Escolha a ferramenta (1 - Qrcode 2 - Gerador de senha)",
+        pattern: "/^[1-2]+$/", //regex (regular expression) que permite ao user escolher entre um ou dois
+        message: "Escolha uma opção váilida",
+        required: true,
+    },
+]
